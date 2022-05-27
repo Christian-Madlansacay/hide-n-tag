a56f4a5359103daee848dc4aa98712a98c1fa095
Christian Madlansacay <74886855+Christian-Madlansacay@users.noreply.github.com>
2022-05-27 17:16:18 -0400

    Added help message if no arguments in main command

--- src/main/java/me/kewlkidchristian/hidentag/Commands/MainCommand.java
+++ src/main/java/me/kewlkidchristian/hidentag/Commands/MainCommand.java
@@ -1,6 +1,11 @@
 package me.kewlkidchristian.hidentag.Commands;
 
 import me.kewlkidchristian.hidentag.HideNTag;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -19,22 +24,37 @@ public class MainCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (args[0].equalsIgnoreCase("start")) {
-            Player[] onlinePlayers = Bukkit.getOnlinePlayers().toArray(new Player[0]);
-            double randomDouble = Math.floor(Math.random()*onlinePlayers.length);
-            int randomInt = (int)randomDouble;
-            Player seeker = onlinePlayers[randomInt];
+        if (args.length >= 1) {
+            if (args[0].equalsIgnoreCase("start")) {
+                Player[] onlinePlayers = Bukkit.getOnlinePlayers().toArray(new Player[0]);
+                double randomDouble = Math.floor(Math.random() * onlinePlayers.length);
+                int randomInt = (int) randomDouble;
+                Player seeker = onlinePlayers[randomInt];
 
-            for (Player player : onlinePlayers) {
-                if (player == seeker) {
-                    player.sendMessage("You are the seeker!");
-                } else {
-                    player.sendMessage("You are a hider!");
+                final Component youAreTheText = Component.text("You are the"); // TODO: Make text use messages.toml
+                final Component seekerText = Component.text("SEEKER!", NamedTextColor.RED, TextDecoration.BOLD);
+                final Component hiderText = Component.text("HIDER!", NamedTextColor.RED, TextDecoration.BOLD);
+
+                final Title seekerTitle = Title.title(youAreTheText, seekerText);
+                final Title hiderTitle = Title.title(youAreTheText, hiderText);
+
+                for (Player player : onlinePlayers) {
+                    if (player == seeker) {
+                        player.showTitle(seekerTitle);
+                        // TODO: Teleport seeker to seeker spawn
+                    } else {
+                        player.showTitle(hiderTitle);
+                        // TODO: Teleport hider to map spawn
+                    }
                 }
             }
         } else {
             if (sender instanceof Player player) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "geowilzu stinks &b&lreally badly."));
+                var miniMessage = MiniMessage.miniMessage();
+
+                Component parsed = miniMessage.deserialize(plugin.messages.getString("help"));
+
+                player.sendMessage(parsed);
             }
         }
 
